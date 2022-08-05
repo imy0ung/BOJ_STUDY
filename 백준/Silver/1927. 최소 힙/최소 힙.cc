22d3647c3be4e4e308f833bf9d3339ed86377308
imy0ung <105477780+imy0ung@users.noreply.github.com>
2022-08-05 16:50:36 +0900

    [Silver II] Title: 최소 힙, Time: 20 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -1,32 +1,64 @@
 #include <iostream>
+#include <cstdio>
+#include <algorithm>
 #include <vector>
 #include <stack>
 #include <queue>
-#include <cmath>
-#include <algorithm>
-#include<cstring>
 #pragma warning (disable : 4996)
 
 using namespace std;
 
-int main() {
-	int n;
-	int x;
-	cin >> n;
-	priority_queue <int> pq;
+int N;
+int heap[100002];
+int sz = 0;
 
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &x);
-		if (x != 0)
-			pq.push(-x);
-		else {
-			if (!pq.empty()) {
-				int ans = pq.top();
-				printf("%d\n", -ans);
-				pq.pop();
+void push(int x) {
+	heap[++sz] = x;
+	int idx = sz;
+	while (idx != 1) {
+		int parent = idx / 2;
+		if (heap[parent] <= heap[idx]) break;
+		swap(heap[parent], heap[idx]);
+		idx = parent;
+	}
+}
+
+int top() {
+	return heap[1];
+}
+
+void pop() {
+	heap[1] = heap[sz--];
+	int idx = 1;
+	while (2 * idx <= sz) {
+		int lc = 2 * idx, rc = 2 * idx + 1;
+		int min_child = lc;
+		if (rc <= sz && heap[rc] < heap[lc])
+			min_child = rc;
+		if (heap[idx] <= heap[min_child]) break;
+		swap(heap[idx], heap[min_child]);
+		idx = min_child;
+	}
+}
+
+void test() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		int x; scanf("%d", &x);
+
+		if (x == 0) {
+			if (sz > 0) {
+				printf("%d\n", top());
+				pop();
 			}
-			else
-				printf("0\n");
+			else printf("0\n");
+		}
+		else {
+			push(x);
 		}
 	}
+}
+
+int main(void) {
+	test();
 }
