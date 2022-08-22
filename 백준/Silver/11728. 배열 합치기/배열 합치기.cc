45dc32b9ad6d734d2c1ba24e5e5ed06af6a97df6
imy0ung <105477780+imy0ung@users.noreply.github.com>
2022-08-22 16:10:51 +0900

    [Silver V] Title: 배열 합치기, Time: 512 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -11,48 +11,20 @@
 using namespace std;
 
 int n, m;
-int arr1[1000000];
-int arr2[1000000];
-int sorted_arr[2000000];
-
-void merge_sort() {
-	int i = 0; int j = 0; int k = 0;
-	while (i < n && j < m) {
-		if (arr1[i] < arr2[j]) {
-			sorted_arr[k] = arr1[i];
-			i++;
-		}
-		else {
-			sorted_arr[k] = arr2[j];
-			j++;
-		}
-		k++;
-	}
-	// 남은거 털어주기
-	if (i < n) {
-		for (int x = i; x < n; x++) {
-			sorted_arr[k] = arr1[x];
-			k++;
-		}
-	}
-	if (j < m) {
-		for (int x = j; x < m; x++) {
-			sorted_arr[k] = arr2[x];
-			k++;
-		}
-	}
-}
+int a[1000005], b[1000005], c[2000005];
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
 	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-		cin >> arr1[i];
-	for (int i = 0; i < m; i++)
-		cin >> arr2[i];
-	merge_sort();
-	for (int i = 0; i < n+m; i++)
-		cout << sorted_arr[i] << ' ';
+	int aidx = 0, bidx = 0;
+	for (int i = 0; i < n; i++) cin >> a[i];
+	for (int i = 0; i < m; i++) cin >> b[i];
+	for (int i = 0; i < n + m; i++) {
+		if (aidx == n) c[i] = b[bidx++]; // 남은거 털어주기1
+		else if (bidx == m) c[i] = a[aidx++]; // 남은거 털어주기2
+		else if (a[aidx] <= b[bidx]) c[i] = a[aidx++]; // 비교
+		else c[i] = b[bidx++]; // 비교
+	}
+	for (int i = 0; i < n + m; i++) cout << c[i] << ' '; // 출력
 }
