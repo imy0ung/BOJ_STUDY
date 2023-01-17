ac927abdd62589b32d9c49cf05d127736eb001ee
imy0ung <105477780+imy0ung@users.noreply.github.com>
2023-01-17 16:48:07 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,46 +1,41 @@
 #include <iostream>
-#include <cstdio>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include <stack>
 #include <queue>
-#pragma warning (disable : 4996)
+#include <string>
+#include <list>
+
+#define X first
+#define Y second
 
 using namespace std;
-#define MAX 1000
 
-int map[MAX][MAX];
-int dis[MAX][MAX];
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
+string board[105];
+int dis[105][105]; // 거리
+int dx[4] = {1, 0 ,-1, 0}; // 행
+int dy[4] = {0, 1, 0, -1}; // 열
+int n, m;
 
 int main(void) {
-	int n, m;
 	cin >> n >> m;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++)
-			scanf("%1d", &map[i][j]);
-	}
-	for (int i = 1; i <= n; i++) {
-		fill(dis[i], dis[i + m + 1], -1);
-	}
-
-	queue<pair<int, int>> q;
-	q.push({ 1,1 }); dis[1][1] = 0;
-
-	while (!q.empty()) {
-		auto cur = q.front(); q.pop();
+	for (int i = 0; i < n; i++)
+		cin >> board[i];
+	for (int i = 0; i < n; i++) fill(dis[i], dis[i] + m, - 1);
+	queue<pair<int, int>> Q;
+	dis[0][0] = 0;
+	Q.push({ 0,0 });
+	while (!Q.empty()) {
+		auto cur = Q.front();
+		Q.pop();
 		for (int i = 0; i < 4; i++) {
-			int nx = cur.first + dx[i];
-			int ny = cur.second + dy[i];
-			if (nx < 1 || nx >= n + 1 || ny < 1 || ny >= m + 1) continue;
-			if (dis[nx][ny] > -1 || map[nx][ny] == 0) continue;
-			q.push({ nx,ny });
-			dis[nx][ny] = dis[cur.first][cur.second] + 1;
+			int nx = cur.X + dx[i];
+			int ny = cur.Y + dy[i];
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+			if (board[nx][ny] == '0' || dis[nx][ny] >= 0) continue;
+			dis[nx][ny] = dis[cur.X][cur.Y] + 1;
+			Q.push({ nx,ny });
 		}
 	}
-
-	cout << dis[n][m] + 1;
-
-	return 0; 
+	cout << dis[n - 1][m - 1] + 1;
 }
