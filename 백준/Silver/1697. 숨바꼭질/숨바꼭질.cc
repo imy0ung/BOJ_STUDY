7e11a6dad1fdb8be4f79b7d7a286e037094f1f66
imy0ung <105477780+imy0ung@users.noreply.github.com>
2023-01-18 16:52:18 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,44 +1,34 @@
 #include <iostream>
-#include <cstdio>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include <stack>
 #include <queue>
-#pragma warning (disable : 4996)
+#include <string>
+#include <list>
+
+#define X first
+#define Y second
 
 using namespace std;
-#define MAX 1000
-
-int visit[200001];
-int mv[2] = { -1,1 };
-int N, K;
+int dist[100002];
+int n, k;
+queue<int> Q;
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
-	cin >> N >> K; // 수빈의 현위치와 동생의 현위치
-	queue<int> Q;
-	fill(visit, visit + 200001, -1);
-	Q.push(N); visit[N] = 0;
-
-	while (!Q.empty()) {
-		int cur = Q.front(); Q.pop();
-		for (int dir = 0; dir < 2; dir++) {
-			int mover = cur + mv[dir];
-			if (mover < 0 || mover > 200000 || visit[mover] > -1) continue;
-			Q.push(mover);
-			visit[mover] = visit[cur] + 1;
-		} // 왼쪽 오른쪽
-
-		int mover = cur * 2;
-		if (mover < 0 || mover > 200000 || visit[mover] > -1) continue;
-		Q.push(mover);
-		visit[mover] = visit[cur] + 1; // 순간이동하는 경우도 
-		
-		if (mover == K) {
-			break;
-		} // 찾으면 break;
+	cin >> n >> k;
+	fill(dist, dist + 100001, -1);
+	dist[n] = 0; 
+	Q.push(n);
+	while (dist[k] == -1) {
+		auto cur = Q.front(); Q.pop();
+		for (auto t : { cur + 1, cur - 1, cur * 2 }) {
+			if (t < 0 || t > 100000) continue;
+			if (dist[t] != -1) continue;
+			dist[t] = dist[cur] + 1;
+			Q.push(t);
+		}
 	}
-	cout << visit[K];
+	cout << dist[k];
 }
