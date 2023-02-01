0db902f664bb4fdef3fd626e1887d8cb6eb2b51a
imy0ung <105477780+imy0ung@users.noreply.github.com>
2023-02-01 16:51:12 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,42 +1,43 @@
 #include <iostream>
-#include <cstdio>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include <stack>
 #include <queue>
-#include <unordered_set>
-#include <unordered_map>
-#pragma warning (disable : 4996)
+#include <string>
+#include <list>
 
+#define X first
+#define Y second
+// 보통 1억에 1초 
+// TC가 10만기준으로 O(N)은 1/1000초
+// TC가 10만기준으로 O(N^2)은 100억 , 100초
+// TC가 105*105
 using namespace std;
 
-int n, m; 
-int arr[10]; // 수열을 담는 배열
-bool isused[10]; // 특정 수가 쓰였는지
+int n, m; // 입력으로 주어진 값
+int arr[10]; // 수열에 담을 배율
+bool isused[10]; // 특정수가 쓰였는가
 
-void func(int k) { //func(k)는 k개까지의 수를 선택한 상황에서 arr[k]를 정하는 함수
+void func(int k) {
 	if (k == m) {
-		for (int i = 0; i < m; i++) {
+		for (int i = 0; i < m; i++)
 			cout << arr[i] << ' ';
-		}
 		cout << '\n';
 		return;
-	} // base condition, m개를 택했으니 종료
+	} // m개가 택해서 종료 조건
 	for (int i = 1; i <= n; i++) {
-		if (!isused[i]) {
-			arr[k] = i; // k번째에 i 집어넣고
-			isused[i] = 1; // 방문처리
-			func(k + 1); // k+1번째 일때도 실행
-			isused[i] = 0; // 다시 방문처리해제
+		if (!isused[i]) { // 아직쓰이지않았다면
+			arr[k] = i;
+			isused[i] = 1; // 사용함으로 선언하고 
+			func(k + 1); // 다음행으로 넘겨줌
+			isused[i] = 0; // 사용되지 않음
 		}
 	}
 }
 
-int main(void) { 
+int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
 	cin >> n >> m;
 	func(0);
 }
-// 백트래킹 : 현재 상태에서 가능한 모든 후보군을 따라 들어가며 탐색하는 알고리즘
