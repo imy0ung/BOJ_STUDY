65c9a5762c9189c6466a4862cf697e0de2660d90
imy0ung <105477780+imy0ung@users.noreply.github.com>
2023-07-13 16:16:48 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,22 +1,20 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include<string>
+#include <bits/stdc++.h>
+#pragma warning (disable:4996)
 
 using namespace std;
 
 int arr[26];
-// iterator = container 내의 포인터
-// refernce = 일종의 별명, 변수를 통해 같은 공간을 써서 메모리가 차지 x
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+
 	string s; 
 	cin >> s;
-	for (char e : s)
+
+	for (auto e : s)
 		arr[e - 'a']++;
-	for (int e : arr)
+	for (auto e : arr)
 		cout << e << ' ';
 }
