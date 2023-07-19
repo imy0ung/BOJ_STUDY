aaa8e1a619f0c808cad9022d8a65f8f6bed9d468
imy0ung <105477780+imy0ung@users.noreply.github.com>
2023-07-19 15:27:24 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -1,45 +1,49 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <string>
-#include <list>
+#include <bits/stdc++.h>
+#pragma warning (disable:4996)
 
 using namespace std;
 
-// iterator = container 내의 포인터
-// refernce = 일종의 별명, 변수를 통해 같은 공간을 써서 메모리가 차지 x
-// range - based for loop
-// getlin(cin,s) -> 공백을 포함해서 받음
-
-
 int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
 	while (1) {
-		string a;
-		getline(cin, a);
-		if (a == ".") break;
-		stack<char> s;
-		bool isValid = true;
-		for (auto c : a) {
-			if (c == '(' || c == '[') s.push(c);
-			else if (c == ')') {
-				if (s.empty() || s.top() != '(') {
-					isValid = false;
-					break; // empty 안넣으면 컴파일에러가 남
-				} // 같은 괄호인가 1
-				s.pop(); // 조건 부합시 pop
+		stack<int> stack;
+		string s;
+		int flag = 0;
+		getline(cin,s);
+		if (s == ".") break;
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == '(' || s[i] == '[')
+				stack.push(s[i]);
+			else if (s[i] == ')') {
+				if (stack.empty()) {
+					flag = 1; break;
+				}
+				else if (stack.top() == '(')
+					stack.pop();
+				else {
+					flag = 1; break;
+				}
 			}
-			else if (c == ']') {
-				if (s.empty() || s.top() != '[') {
-					isValid = false;
-					break;
-				} // 같은 괄호인가 2
-				s.pop(); // 조건 부합시 pop
+			else if (s[i] == ']') {
+				if (stack.empty()) {
+					flag = 1; break;
+				}
+				else if (stack.top() == '[')
+					stack.pop();
+				else {
+					flag = 1; break; // (])
+				}
 			}
 		}
-		if (!s.empty()) isValid = false; // 스택이 안비어있으면 false
-		if (isValid) cout << "yes\n";
-		else cout << "no\n";
+
+		if (flag == 1)
+			cout << "no\n";
+		else if (stack.empty())
+			cout << "yes\n";
+		else
+			cout << "no\n";
 	}
 }
