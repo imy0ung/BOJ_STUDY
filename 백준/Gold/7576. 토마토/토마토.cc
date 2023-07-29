a59ac71d6eed0ee3a6b7e842c1a1b6b9944fa0f7
imy0ung <105477780+imy0ung@users.noreply.github.com>
2023-07-29 14:49:56 +0900

    [Gold V] Title: 토마토, Time: 96 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,56 +1,54 @@
 #include <iostream>
-#include <cstdio>
-#include <algorithm>
-#include <vector>
-#include <stack>
-#include <queue>
-#pragma warning (disable : 4996)
+#include <bits/stdc++.h>
+#pragma warning (disable:4996)
+#define X first
+#define Y second
 
 using namespace std;
-#define MAX 1005
 
-int board[MAX][MAX];
-int dis[MAX][MAX];
+int board[1002][1002];
+int dist[1002][1002];
 int dx[4] = { 1,0,-1,0 };
 int dy[4] = { 0,1,0,-1 };
-queue<pair<int, int>> Q;
+int ans;
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+
 	int n, m;
-	cin >> n >> m;
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
+	queue<pair<int, int>> Q;
+	cin >> m >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
 			cin >> board[i][j];
-			if (board[i][j] == 1)
+			if (board[i][j] == 1) {
 				Q.push({ i,j });
-			if (board[i][j] == 0)
-				dis[i][j] = -1;
+				dist[i][j] = 0;
+			}
+			else if (board[i][j] == 0)
+				dist[i][j] = -1;
 		}
 	}
-
 	while (!Q.empty()) {
 		auto cur = Q.front(); Q.pop();
 		for (int dir = 0; dir < 4; dir++) {
 			int nx = cur.first + dx[dir];
 			int ny = cur.second + dy[dir];
-			if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue; // 범위 초과 제한
-			if (board[nx][ny] == -1 || dis[nx][ny] >= 0) continue; // 방문하거나, 벽이거나
-			dis[nx][ny] = dis[cur.first][cur.second] + 1;
-			Q.push({ nx,ny }); // 넣어주고 반복
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+			if (board[nx][ny] == -1 || dist[nx][ny] >= 0) continue;
+			Q.push({ nx,ny });
+			dist[nx][ny] = dist[cur.X][cur.Y] + 1;
 		}
 	}
-	int ans = 0;
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			if (dis[i][j] == -1) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (dist[i][j] == -1) {
 				cout << -1;
 				return 0;
 			}
-			ans = max(dis[i][j], ans);
+			ans = max(ans, dist[i][j]);
 		}
 	}
-
 	cout << ans;
 }
