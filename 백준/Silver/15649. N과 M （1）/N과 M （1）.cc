61e68f1efaf501d536acfcea8926460bb37dacec
imy0ung <105477780+imy0ung@users.noreply.github.com>
2023-08-17 17:12:14 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,42 +1,32 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <string>
-#include <list>
+#include <bits/stdc++.h>
+#pragma warning (disable:4996)
 
-#define X first
-#define Y second
-// 보통 1억에 1초 
-// TC가 10만기준으로 O(N)은 1/1000초
-// TC가 10만기준으로 O(N^2)은 100억 , 100초
-// TC가 105*105
 using namespace std;
 
-int n, m; // 입력으로 주어진 값
-int arr[10]; // 수열에 담을 배율
-bool isused[10]; // 특정수가 쓰였는가
+int n, m;
+int arr[10];
+int isused[10];
 
-void func(int k) {
-	if (k == m) {
-		for (int i = 0; i < m; i++)
+void func(int k) { //k번째까지
+	if (k == m) { //모든 트리를 탐색했다면 출력
+		for (int i = 0; i < k; i++)
 			cout << arr[i] << ' ';
 		cout << '\n';
-		return;
-	} // m개가 택해서 종료 조건
-	for (int i = 1; i <= n; i++) {
-		if (!isused[i]) { // 아직쓰이지않았다면
-			arr[k] = i;
-			isused[i] = 1; // 사용함으로 선언하고 
-			func(k + 1); // 다음행으로 넘겨줌
-			isused[i] = 0; // 사용되지 않음
+		return; // 탐색 끝났으면 종료
+	}
+	for (int i = 1; i <= n; i++) { // n번째 수까지 순회하면서
+		if (!isused[i]) { // 만약 사용하지 않았다면
+			arr[k] = i; // 삽입
+			isused[i] = 1; // 사용처리해주고
+			func(k + 1); // 더 깊숙히 탑색
+			isused[i] = 0; // func가 끝나면 순회 종료니 다시, 0으로해줌
 		}
 	}
 }
 
 int main(void) {
-	ios::sync_with_stdio(0);
+	iostream::sync_with_stdio(0);
 	cin.tie(0);
 	cin >> n >> m;
 	func(0);
