58bb32d2e409ec1a4b78941014dca69be5ecc67a
imy0ung <105477780+imy0ung@users.noreply.github.com>
2023-08-23 16:22:29 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -5,24 +5,25 @@
 using namespace std;
 
 int n, m;
-int arr[10];
 int isused[10];
+int arr[10];
 
-void func(int k) { //k번째까지
-	if (k == m) { //모든 트리를 탐색했다면 출력
-		for (int i = 0; i < k; i++)
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++)
 			cout << arr[i] << ' ';
 		cout << '\n';
-		return; // 탐색 끝났으면 종료
+		return;
 	}
-	for (int i = 1; i <= n; i++) { // n번째 수까지 순회하면서
-		if (!isused[i]) { // 만약 사용하지 않았다면
-			arr[k] = i; // 삽입
-			isused[i] = 1; // 사용처리해주고
-			func(k + 1); // 더 깊숙히 탑색
-			isused[i] = 0; // func가 끝나면 순회 종료니 다시, 0으로해줌
+	for (int i = 1; i <= n; i++) {
+		if (isused[i] == 0) {
+			arr[k] = i;
+			isused[i] = 1;
+			func(k + 1);
+			isused[i] = 0;
 		}
 	}
+	return;
 }
 
 int main(void) {
