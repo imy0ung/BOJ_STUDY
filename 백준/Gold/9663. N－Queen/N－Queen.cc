5da0220fdaa2cd0663623a65d351822a56251c6c
imy0ung <105477780+imy0ung@users.noreply.github.com>
2023-08-23 16:56:54 +0900

    [Gold IV] Title: N-Queen, Time: 1624 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,42 +1,35 @@
 #include <iostream>
-#include <cstdio>
-#include <algorithm>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <unordered_set>
-#include <unordered_map>
-#pragma warning (disable : 4996)
+#include <bits/stdc++.h>
+#pragma warning (disable:4996)
 
 using namespace std;
 
-bool isused1[40];
-bool isused2[40];
-bool isused3[40];
+int n, cnt;
+int isused1[40];
+int isused2[40];
+int isused3[40];
 
-int cnt = 0;
-int n;
-
-void func(int cur) {
-	if (cur == n) {
+void func(int k) {
+	if (k == n) {
 		cnt++;
 		return;
 	}
 	for (int i = 0; i < n; i++) {
-		if (isused1[i] || isused2[i + cur] || isused3[cur - i + n - 1])
-			continue; 
+		if (isused1[i] || isused2[i + k] || isused3[k - i + n - 1]) continue;
 		isused1[i] = 1;
-		isused2[i + cur] = 1;
-		isused3[cur - i + n - 1] = 1;
-		func(cur + 1);
+		isused2[i + k] = 1;
+		isused3[k - i + n - 1] = 1;
+		func(k + 1);
 		isused1[i] = 0;
-		isused2[i + cur] = 0;
-		isused3[cur - i + n - 1] = 0;
+		isused2[i + k] = 0;
+		isused3[k - i + n - 1] = 0;
 	}
 }
 
-int main(void) { 
-	ios::sync_with_stdio(0);
+int main(void) {
+	iostream::sync_with_stdio(0);
 	cin.tie(0);
-	cin >> n; func(0); cout << cnt;
+	cin >> n;
+	func(0);
+	cout << cnt;
 }
