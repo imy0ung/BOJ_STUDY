f5c42c4be3dcdae87de7f6a2b548cc5d2f3a003c
imy0ung <105477780+imy0ung@users.noreply.github.com>
2023-08-25 16:37:04 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#pragma warning (disable:4996)
+
+using namespace std;
+// 1 2 5 6
+
+int n, m;
+int ans[10];
+int arr[10];
+int isused[10000];
+
+void func(int k, int st) {
+	if (k == m) {
+		for (int i = 0; i < m; i++)
+			cout << ans[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	for (int i = st; i < n; i++) {
+		if (!isused[arr[i]]) {
+			ans[k] = arr[i];
+			func(k + 1, i);
+		}
+	}
+}
+
+int main(void) {
+	iostream::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	sort(arr, arr + n);
+	func(0, 0);
+}
