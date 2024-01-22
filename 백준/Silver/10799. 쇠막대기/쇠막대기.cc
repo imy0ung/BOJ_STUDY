b07ba116e858613dbb01048caf4c7c80371385a8
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-01-22 23:17:46 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -1,37 +1,30 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <string>
-#include <list>
-
+#include <bits/stdc++.h>
+#pragma warning (disable:4996)
 using namespace std;
 
-// iterator = container 내의 포인터
-// refernce = 일종의 별명, 변수를 통해 같은 공간을 써서 메모리가 차지 x
-// range - based for loop
-// getlin(cin,s) -> 공백을 포함해서 받음
+// getline(cin,string s) => 공백을 포함한 문자 받기
+// list- insert는 앞부분, iterator 햇갈리지말자,,
+// deque vs vector, 메모리 연속성 차이, deque이 상위호환느낌
 
 int main(void) {
-	string a;
-	stack<char> s;
-	long long ans = 0;
-	cin >> a;
-	for (int idx = 0; idx < a.size(); idx++) {
-		if (a[idx] == '(') s.push(a[idx]);
-		else if (a[idx] == ')') {
-			if (idx > 0 && a[idx - 1] == '(') {
-				s.pop(); 
-				ans += s.size();
-			}
-			else { // 마지막 막대는 잘린거랑 다름없음으로...!!
-				s.pop();
-				ans++;
-			}
-		}
-	}
-	cout << ans;
+    iostream::sync_with_stdio(0);
+    cin.tie(0);
+    stack<char> s;
+    int ans = 0;
+    string str; cin >> str;
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == '(') s.push(str[i]);
+        else if (str[i] == ')') {
+            if (str[i-1] == '(') {
+                s.pop();
+                ans += s.size();
+            }
+            else {
+                s.pop();
+                ans++;
+            }
+        }
+    }
+    cout << ans;
 }
-// 10799
-// 2504
