b4b9097cd44a8e71f3216342728778765c72632d
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-02-17 17:12:41 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -1,40 +1,41 @@
 #include <iostream>
 #include <bits/stdc++.h>
 #pragma warning (disable:4996)
-
 using namespace std;
-// 1 2 5 6
+using ll = long long;
+// getline(cin,string s) => 공백을 포함한 문자 받기
+// list- insert는 앞부분, iterator 햇갈리지말자,,
+// deque vs vector, 메모리 연속성 차이, deque이 상위호환느낌
 
 int n, m;
 int isused[10];
 int arr[10];
-int ans[10];
+int inp[10];
 
 void func(int k) {
-	if (k == m) {
-		for (int i = 0; i < m; i++)
-			cout << ans[i] << ' ';
-		cout << '\n';
-		return;
-	}
-	int tmp = 0;
-	for (int i = 0; i < n; i++) {
-		if (!isused[i] && tmp != arr[i]) {
-			isused[i] = 1;
-			ans[k] = arr[i];
-			tmp = arr[i];
-			func(k + 1);
-			isused[i] = 0;
-		}
-	}
+    if (k == m) {
+        for (int i = 0; i < m; i++)
+            cout << inp[arr[i]] << ' ';
+        cout << '\n';
+        return;
+    }
+    int tmp = 0;
+    for (int i = 0; i < n; i++) {
+        if (!isused[i] && tmp != inp[i]) {
+            arr[k] = i;
+            isused[i] = 1;
+            tmp = inp[i];
+            func(k + 1);
+            isused[i] = 0;
+        }
+    }
 }
 
 int main(void) {
-	iostream::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-	sort(arr, arr + n);
-	func(0);
+    iostream::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) cin >> inp[i];
+    sort(inp, inp + n);
+    func(0);
 }
