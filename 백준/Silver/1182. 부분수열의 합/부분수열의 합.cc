8b9f9b46b2f60727def51325bd08c899c10d654b
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-02-19 15:31:51 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,40 +1,28 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <string>
-#include <list>
+#include <bits/stdc++.h>
+#pragma warning (disable:4996)
 
-#define X first
-#define Y second
-// 보통 1억에 1초 
-// TC가 10만기준으로 O(N)은 1/1000초
-// TC가 10만기준으로 O(N^2)은 100억 , 100초
-// TC가 105*105
 using namespace std;
 
-int N, S;
-int cnt;
-int arr[30];
+int n, s, cnt;
+int arr[100];
 
 void func(int k, int tot) {
-	if (k == N) {
-		if (tot == S) cnt++;
+	if (k == n) {
+		if (tot == s) cnt++;
 		return;
 	}
-	func(k + 1, tot); // 왼쪽 : 추가안한거
-	func(k + 1, tot + arr[k]); // 오른쪽 : 추가한거
+	func(k + 1, tot); // 수를 선택하지 않은 경우
+	func(k + 1, arr[k] + tot); // 수를 선택한 경우
 }
 
 int main(void) {
-	ios::sync_with_stdio(0);
+	iostream::sync_with_stdio(0);
 	cin.tie(0);
-
-	cin >> N >> S;
-	for (int i = 0; i < N; i++)
+	cin >> n >> s;
+	for (int i = 0; i < n; i++)
 		cin >> arr[i];
 	func(0, 0);
-	if (S == 0) cnt--;
+	if (s == 0) cnt--;
 	cout << cnt;
 }
