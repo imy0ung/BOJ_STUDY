f78daebb5ea77ac172db109168126fed15855241
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-02-23 14:43:37 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#pragma warning (disable:4996)
+using namespace std;
+using ll = long long;
+// getline(cin,string s) => 공백을 포함한 문자 받기
+// list- insert는 앞부분, iterator 햇갈리지말자,,
+// deque vs vector, 메모리 연속성 차이, deque이 상위호환느낌
+
+int n;
+int R[1005];
+int G[1005];
+int B[1005];
+int d[1005][4];
+
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> R[i] >> G[i] >> B[i];
+   
+    d[1][1] = R[1]; d[1][2] = G[1]; d[1][3] = B[1];
+
+    for (int i = 2; i <= n; i++) {
+        d[i][1] = min(d[i - 1][2], d[i - 1][3]) + R[i];
+        d[i][2] = min(d[i - 1][1], d[i - 1][3]) + G[i];
+        d[i][3] = min(d[i - 1][1], d[i - 1][2]) + B[i];
+    }
+    cout << min(min(d[n][1], d[n][2]),d[n][3]);
+}
