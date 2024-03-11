f65fc7483fc437731c18bc1d425c202d9520fa23
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-03-11 11:29:48 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#pragma warning (disable:4996)
+using namespace std;
+using ll = long long;
+// getline(cin,string s) => 공백을 포함한 문자 받기
+// list- insert는 앞부분, iterator 햇갈리지말자,,
+// deque vs vector, 메모리 연속성 차이, deque이 상위호환느낌
+
+int n, ans;
+int a[55]; 
+int b[55];
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> b[i];
+    sort(a, a + n);
+    sort(b, b + n);
+    for (int i = 0; i < n; i++)
+        ans += a[i] * b[n - i - 1];
+    cout << ans;
+} 
