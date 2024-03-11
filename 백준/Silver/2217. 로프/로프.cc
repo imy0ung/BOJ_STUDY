32a3a66979fcfef78524f11a60e56bc0f165ea41
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-03-11 11:20:14 +0900

    [Silver IV] Title: 로프, Time: 12 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#pragma warning (disable:4996)
+using namespace std;
+using ll = long long;
+// getline(cin,string s) => 공백을 포함한 문자 받기
+// list- insert는 앞부분, iterator 햇갈리지말자,,
+// deque vs vector, 메모리 연속성 차이, deque이 상위호환느낌
+
+int n, ans;
+int arr[1000005];
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr + n);
+    for (int i = 1; i <= n; i++) {
+        ans = max(ans, arr[n - i] * i); // 매 순간
+    }
+    cout << ans;
+} 
