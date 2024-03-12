7fec6487ae31ff8496cfbe698ba32d0ed53c2720
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-03-12 11:23:06 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#pragma warning (disable:4996)
+using namespace std;
+using ll = long long;
+// getline(cin,string s) => 공백을 포함한 문자 받기
+// list- insert는 앞부분, iterator 햇갈리지말자,,
+// deque vs vector, 메모리 연속성 차이, deque이 상위호환느낌
+
+int n, ans;
+int arr[1005];
+int d[1005];
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr + n);
+    d[0] = arr[0]; 
+    ans = d[0];
+    for (int i = 1; i < n; i++) {
+        d[i] = d[i - 1] + arr[i];
+        ans += d[i];
+    }
+    cout << ans;
+}
