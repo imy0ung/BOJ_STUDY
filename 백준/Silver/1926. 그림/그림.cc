c0dbc1dcb1e7f8fc27f4365d5743f7cabeb358c6
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-03-21 13:34:40 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,47 +1,53 @@
 #include <iostream>
 #include <bits/stdc++.h>
 #pragma warning (disable:4996)
-#define X first
-#define Y second
-
 using namespace std;
 
-int MAP[505][505];
-int vis[505][505];
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0 , -1 };
-int num, mx;
+// getline(cin,string s) => 공백을 포함한 문자 받기
+// list- insert는 앞부분, iterator 햇갈리지말자,,
+// deque vs vector, 메모리 연속성 차이, deque이 상위호환느낌
+
+int board[501][501];
+int vis[501][501];
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,-1,0,1 };
+int n, m;
+int ans;
+int cnt;
+queue<pair<int, int>>Q;
 
 int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n, m;
-	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			cin >> MAP[i][j];
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (MAP[i][j] == 0 || vis[i][j]) continue;
-			num++;
-			queue <pair<int, int>> Q;
-			int sqr = 0;
-			Q.push({ i,j });
-			vis[i][j] = 1;
-			while (!Q.empty()) {
-				sqr++;
-				auto cur = Q.front(); Q.pop();
-				for (int dir = 0; dir < 4; dir++) {
-					int nx = cur.X + dx[dir];
-					int ny = cur.Y + dy[dir];
-					if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-					if (vis[nx][ny] || MAP[nx][ny] == 0) continue;
-					Q.push({ nx, ny });
-					vis[nx][ny] = 1;
-				}
-			}
-			mx = max(sqr, mx);
-		}
-	}
-	cout << num << '\n' << mx;
+    iostream::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> board[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (board[i][j] == 1 && vis[i][j] != 1) {
+                int area = 0;
+                Q.push({ i,j });
+                vis[i][j] = 1;
+                while (!Q.empty()) {
+                    auto cur = Q.front(); Q.pop();
+                    area++;
+                    for (int dir = 0; dir < 4; dir++) {
+                        int nx = cur.first + dx[dir];
+                        int ny = cur.second + dy[dir];
+                        if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                        if (vis[nx][ny] == 1 || board[nx][ny] != 1) continue;
+                        vis[nx][ny] = 1;
+                        Q.push({ nx,ny });
+                    }
+                }
+                cnt++;
+                ans = max(area, ans);
+            }
+        }
+    }
+    cout << cnt << '\n' << ans;
+    return 0;
 }
