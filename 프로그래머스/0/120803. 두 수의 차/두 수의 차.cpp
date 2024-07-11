d13e141bfb9dbfb43d3400a810fd9d7282c5e6d8
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-07-11 21:52:51 +0900

    [level 0] Title: 두 수의 차, Time: 0.03 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    cin >> num1 >> num2;
+    answer = num1 - num2;
+    return answer;
+}
