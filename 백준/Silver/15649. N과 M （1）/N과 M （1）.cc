32f19d58600c72e48f8b96a99cb51b0206e65756
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-07-13 13:44:44 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,33 +1,32 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#pragma warning (disable:4996)
-
 using namespace std;
 
 int n, m;
-int isused[10];
 int arr[10];
+int vis[10];
 
 void func(int k) {
 	if (k == m) {
-		for (int i = 0; i < m; i++)
+		for (int i = 0; i < m; i++) {
 			cout << arr[i] << ' ';
+		}
 		cout << '\n';
 		return;
 	}
+
 	for (int i = 1; i <= n; i++) {
-		if (isused[i] == 0) {
+		if (vis[i] != 1) {
 			arr[k] = i;
-			isused[i] = 1;
+			vis[i] = 1;
 			func(k + 1);
-			isused[i] = 0;
+			vis[i] = 0;
 		}
 	}
-	return;
 }
 
-int main(void) {
-	iostream::sync_with_stdio(0);
+int main() {
+	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cin >> n >> m;
 	func(0);
