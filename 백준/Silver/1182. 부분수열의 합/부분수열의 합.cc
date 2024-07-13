fea3445d1253ee6f6f1519605e8afec635741efa
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-07-13 14:29:50 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#pragma warning (disable:4996)
-
 using namespace std;
 
-int n, s, cnt;
-int arr[100];
+int n, s;
+int arr[30];
+int vis[30];
+int cnt;
 
 void func(int k, int tot) {
 	if (k == n) {
 		if (tot == s) cnt++;
 		return;
 	}
-	func(k + 1, tot); // 수를 선택하지 않은 경우
-	func(k + 1, arr[k] + tot); // 수를 선택한 경우
+	func(k + 1, tot);
+	func(k + 1, tot + arr[k]);
 }
 
-int main(void) {
-	iostream::sync_with_stdio(0);
+int main() {
+	ios::sync_with_stdio(0);
 	cin.tie(0);
+
 	cin >> n >> s;
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
+	for (int i = 0; i < n; i++) cin >> arr[i];
 	func(0, 0);
 	if (s == 0) cnt--;
 	cout << cnt;
