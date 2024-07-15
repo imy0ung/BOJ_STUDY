98e90719a034d986e80f845e010ac15db53d95b0
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-07-15 15:46:09 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4484 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -1,39 +1,27 @@
 #include <iostream>
-#include <cstdio>
 #include <algorithm>
-#include <vector>
 #include <stack>
 #include <queue>
-#include <unordered_set>
-#include <unordered_map>
-#pragma warning (disable : 4996)
 
 using namespace std;
 
-int postive_freq[1000001];
-int negative_freq[1000001];
-int arr[1000001];
-int N;
+int freq[2000005];
+int n;
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> arr[i];
-		if (arr[i] >= 0) postive_freq[arr[i]]++;
-		if (arr[i] < 0) negative_freq[-arr[i]]++;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		x += 1000000;
+		freq[x]++;
 	}
-	for (int i = 1000000; i > 0; i--) {
-		if (negative_freq[i] != 0) {
-			for (int j = 0; j < negative_freq[i]; j++)
-				cout << -i << '\n';
-		}
-	}
-	for (int i = 0; i < 1000001; i++) {
-		if (postive_freq[i] != 0) {
-			for (int j = 0; j < postive_freq[i]; j++)
-				cout << i << '\n';
+	for (int i = 0; i < 2000005; i++) {
+		while (freq[i] > 0) {
+			cout << i - 1000000 << '\n';
+			freq[i]--;
 		}
 	}
 }
