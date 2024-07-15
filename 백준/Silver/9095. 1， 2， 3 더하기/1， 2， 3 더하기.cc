b1bed3956e120a072347771c1af6193274897f5c
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-07-15 20:19:52 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,29 +1,28 @@
 #include <iostream>
-#include <cstdio>
 #include <algorithm>
-#include <vector>
 #include <stack>
 #include <queue>
-#include <unordered_set>
-#include <unordered_map>
-#pragma warning (disable : 4996)
 
 using namespace std;
-
-int d[1000];
+// d[k] = d[k-1] + d[k-2] + 1
+int d[100];
+int n;
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int n; cin >> n;
-	d[1] = 1; d[2] = 2; d[3] = 4;
+	d[1] = 1;
+	d[2] = 2;
+	d[3] = 4;
 
-
-	while(n--) {
-		int x; cin >> x;
-		for (int i = 4; i <= x; i++) {
-			d[i] = d[i - 1] + d[i - 2] + d[i - 3];
-		}
-		cout << d[x] <<'\n';
+	for (int i = 4; i <= 11; i++) {
+		d[i] = d[i - 1] + d[i - 2] + d[i - 3];
 	}
+	cin >> n;
+	while (n--) {
+		int x;
+		cin >> x;
+		cout << d[x] << '\n';
+	}
+
 }
