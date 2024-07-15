26e53aaaf21fd18f70b50334ac230066cf401e14
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-07-15 21:13:16 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,28 +1,29 @@
 #include <iostream>
-#include <bits/stdc++.h>
-#pragma warning (disable:4996)
+#include <algorithm>
+#include <stack>
+#include <queue>
+
 using namespace std;
-using ll = long long;
-// getline(cin,string s) => 공백을 포함한 문자 받기
-// list- insert는 앞부분, iterator 햇갈리지말자,,
-// deque vs vector, 메모리 연속성 차이, deque이 상위호환느낌
 
 int n, m;
-int d[1000005];
-int arr[1000005];
+int arr[100005];
+int d[100005];
 
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++)
-        cin >> arr[i];
-    for (int i = 1; i <= n; i++) {
-        d[i] = d[i - 1] + arr[i];
-    }
-    while (m--) {
-        int a, b;
-        cin >> a >> b;
-        cout << d[b] - d[a - 1] << '\n';
-    }
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) cin >> arr[i];
+	
+	d[0] = arr[0];
+
+	for (int i = 1; i <= n; i++) {
+		d[i] = d[i - 1] + arr[i];
+	}
+
+	while (m--) {
+		int x, y;
+		cin >> x >> y;
+		cout << d[y] - d[x] + arr[x] << '\n';
+	}
 }
