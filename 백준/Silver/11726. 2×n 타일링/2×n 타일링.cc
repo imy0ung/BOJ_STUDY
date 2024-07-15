c14d1f80bc4eab811bcde3325275d7f8d7dacdde
imy0ung <105477780+imy0ung@users.noreply.github.com>
2024-07-15 21:07:13 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,23 +1,20 @@
 #include <iostream>
 #include <algorithm>
-#include <vector> // pair library
 #include <stack>
 #include <queue>
-#include <cmath>
-#pragma warning(disable:4996)
 
-using namespace std; //dynamic program
+using namespace std;
 
-int d[1001];
+int n;
+int d[1005];
 
-int dp(int x) {
-	if (x == 1) return 1;
-	if (x == 2) return 2;
-	if (d[x] != 0) return d[x];
-	return d[x] = (dp(x - 1) + dp(x - 2)) % 10007;
-}
 int main(void) {
-	int x;
-	scanf("%d", &x);
-	printf("%d", dp(x));
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	d[1] = 1;
+	d[2] = 2;
+	cin >> n;
+	for (int i = 3; i <= n; i++)
+		d[i] = (d[i - 1] + d[i - 2]) % 10007;
+	cout << d[n];
 }
