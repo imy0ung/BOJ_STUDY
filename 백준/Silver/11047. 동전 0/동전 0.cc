f0aa10f40ad5e23a11a02108c8213ff03d4eea27
ditto <105477780+imy0ung@users.noreply.github.com>
2024-09-09 15:59:09 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -1,23 +1,19 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#pragma warning (disable:4996)
 using namespace std;
-using ll = long long;
-// getline(cin,string s) => 공백을 포함한 문자 받기
-// list- insert는 앞부분, iterator 햇갈리지말자,,
-// deque vs vector, 메모리 연속성 차이, deque이 상위호환느낌
 
 int n, k, ans;
 int arr[15];
 
-int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n >> k;
-    for (int i = 0; i < n; i++) cin >> arr[i];
-    for (int i = n - 1; i >= 0; i--) {
-        ans += k / arr[i];
-        k %= arr[i];
-    }
-    cout << ans;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	for (int i = n - 1; i >= 0; i--) {
+		ans += k / arr[i];
+		k -= arr[i] * (k/arr[i]);
+	}
+	cout << ans;
 }
