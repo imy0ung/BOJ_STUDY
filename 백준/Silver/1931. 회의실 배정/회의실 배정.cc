edf088c29231d491c1a09ac55d8ce3ea5d804782
ditto <105477780+imy0ung@users.noreply.github.com>
2024-09-10 13:22:31 +0900

    [Silver I] Title: 회의실 배정, Time: 24 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,35 +1,27 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#pragma warning (disable:4996)
 using namespace std;
-using ll = long long;
-// getline(cin,string s) => 공백을 포함한 문자 받기
-// list- insert는 앞부분, iterator 햇갈리지말자,,
-// deque vs vector, 메모리 연속성 차이, deque이 상위호환느낌
 
-int n, ans = 1;
-pair<int, int> t[100005];
+int N, ans, t;
+pair<int, int> arr[100005];
 
-bool compare(const pair<int, int>& a, const pair<int, int>& b) {
-    if (a.second == b.second) {
-        return a.first < b.first;
-    }
-    return a.second < b.second;
+bool compare(pair<int, int>& a, pair<int, int>& b) {
+	if (a.second == b.second) {
+		return a.first < b.first;
+	}
+	return a.second < b.second;
 }
 
-int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> t[i].first >> t[i].second;
-    }
-    sort(t, t + n,compare);
-    int cur = t[0].second;
-    for (int i = 1; i < n; i++) {
-        if (t[i].first >= cur) {
-            cur = t[i].second; ans++;
-        }
-    }
-    cout << ans;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> N;
+	for (int i = 0; i < N; i++) cin >> arr[i].first >> arr[i].second;
+	sort(arr, arr + N, compare);
+	for (int i = 0; i < N; i++) {
+		if (t > arr[i].first) continue;
+		ans++;
+		t = arr[i].second;
+	}
+	cout << ans;
 }
