57120a95272c9293df2618f1383964c231540919
ditto <105477780+imy0ung@users.noreply.github.com>
2024-09-12 14:04:56 +0900

    [Silver IV] Title: 수 찾기, Time: 44 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -2,19 +2,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,k;
-int arr[100005];
+int M, N;
+int arr1[100005];
+int arr2[100005];
+
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cin >> n;
-	for (int i = 0; i < n; i++) cin >> arr[i];
-	sort(arr, arr + n);
-	cin >> k;
-	while (k--) {
-		int f;
-		cin >> f;
-		cout << binary_search(arr, arr + n, f) << '\n';
-	}
+
+	cin >> N;
+	for (int i = 0; i < N; i++) cin >> arr1[i];
+	sort(arr1, arr1 + N);
+	cin >> M;
+	for (int i = 0; i < M; i++) cin >> arr2[i];
+
+	for (int i = 0; i < M; i++) cout << binary_search(arr1,arr1+N,arr2[i]) << '\n';
 }
