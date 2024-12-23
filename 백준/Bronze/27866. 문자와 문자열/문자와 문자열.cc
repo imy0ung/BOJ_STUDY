9fb6733f5ed547fbda73392afd8323b73af0c717
ditto <105477780+imy0ung@users.noreply.github.com>
2024-12-23 17:03:31 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+int n;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> s;
+	cin >> n;
+	cout << s[n-1];
+}
