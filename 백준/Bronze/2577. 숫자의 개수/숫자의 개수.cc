6c0b253887524f8da1179e5ba18650a049c3d15f
ditto <105477780+imy0ung@users.noreply.github.com>
2025-02-12 19:55:08 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -1,22 +1,20 @@
-#include <iostream>
-#include <bits/stdc++.h>
-#pragma warning (disable:4996)
+#include <iostream>	
+#include <algorithm>
+#define ll long long
 
 using namespace std;
 
-int arr[10];
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+int main() {
 	int A, B, C;
-
 	cin >> A >> B >> C;
-	int ans = A * B * C;
-	while (ans >= 1) {
-		arr[ans%10]++;
-		ans = ans / 10;
+	int x = A * B * C;
+	int arr[10] = {};
+
+	while (x != 0) {
+		int k = x % 10;
+		x /= 10;
+		arr[k]++;
 	}
-	for (auto e : arr)
-		cout << e << '\n';
+	for (auto e : arr) cout << e << '\n';
 }
