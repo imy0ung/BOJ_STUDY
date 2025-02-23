ede05d0a5f4d53a886b92397b18631c8132f9463
ditto <105477780+imy0ung@users.noreply.github.com>
2025-02-23 15:04:26 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -1,26 +1,21 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#pragma warning (disable:4996)
-
 using namespace std;
 
-int n;
-queue<int> q;
+int ans;
 
 int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n;
-	for (int i = 1; i <= n; i++)
-		q.push(i);
-	while (1) {
-		if (q.size() == 1) {
-			cout << q.front();
-			break;
-		}
-		q.pop();
-		int top = q.front();
-		q.pop();
-		q.push(top);
-	}
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    queue<int> q;
+    int n; cin >> n;
+    for (int i = 1; i <= n; i++) q.push(i);
+    
+    while (q.size() != 1) {
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    
+    cout << q.front();
 }
