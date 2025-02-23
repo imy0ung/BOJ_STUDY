79fc1f4ba8a3b5a053f33dbbaded3d3f6fb6fde1
ditto <105477780+imy0ung@users.noreply.github.com>
2025-02-23 14:49:35 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -1,46 +1,41 @@
 #include <iostream>
-#include <algorithm>
-#include <stack>
-#include <queue>
-
-// iterator = container's pointer
-// reference = 일종의 별명, 같은 공간, 메모리 차지 x
-// range-based for loop // for (auto e : s)
-
+#include <bits/stdc++.h>
 using namespace std;
 
-queue<int> Q;
-int N, x;
-string s;
+int ans;
 
 int main(void) {
-	cin >> N;
-	while (N--) {
-		cin >> s;
-		if (s == "push") {
-			cin >> x;
-			Q.push(x);
-		}
-		else if (s == "pop") {
-			if (Q.empty()) cout << "-1\n";
-			else {
-				cout << Q.front() << '\n';
-				Q.pop();
-			}
-		}
-		else if (s == "size") {
-			cout << Q.size() << '\n';
-		}
-		else if (s == "empty") {
-			cout << Q.empty() << '\n';
-		}
-		else if (s == "front") {
-			if (Q.empty()) cout << "-1\n";
-			else cout << Q.front() << '\n';
-		}
-		else if (s == "back") {
-			if (Q.empty()) cout << "-1\n";
-			else cout << Q.back() << '\n';
-		}
-	}
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    queue<int> q;
+    int n; cin >> n;
+    for (int i = 0; i < n; i++) {
+        string s; cin >> s;
+        if (s == "push") {
+            int x; cin >> x;
+            q.push(x);
+        }
+        if (s == "pop") {
+            if (!q.empty()) {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+            else cout << -1 << '\n';
+        }
+        if (s == "size") {
+            cout << q.size() << '\n';
+        }
+        if (s == "empty") {
+            if (!q.empty()) cout << 0 << '\n';
+            else cout << 1 << '\n';
+        }
+        if (s == "front") {
+            if (!q.empty()) cout << q.front() << '\n';
+            else cout << -1 << '\n';
+        }
+        if (s == "back") {
+            if (!q.empty()) cout << q.back() << '\n';
+            else cout << -1 << '\n';
+        }
+    }
 }
