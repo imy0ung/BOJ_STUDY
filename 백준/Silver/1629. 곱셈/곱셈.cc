5c77f972b564df4d9f6e0d200c08543890d54694
ditto <105477780+imy0ung@users.noreply.github.com>
2025-03-04 20:20:56 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,27 +1,25 @@
-#include <iostream>
-#include <bits/stdc++.h>
-#pragma warning (disable:4996)
-#define X first
-#define Y second
-#define ll long long int
+#include <iostream>	
+#include <algorithm>
+#include <list>
+#include <stack>
+#include <queue>
+#define ll long long
+
 using namespace std;
 
-ll a, b, c;
+ll A, B, C;
 
-int func(ll a, ll b, ll c) {
-	if (b == 1) return a % c;
-	ll k = func(a, b / 2, c);
-	k = (k * k) % c;
-	if (b % 2 == 0)
-		return k;
-	else 
-		return k * a % c;
+ll func(ll a, ll b, ll m) {
+	if (b == 1) return a % m;
+	ll val = func(a, b / 2, m);
+	val = val * val % m;
+	if (b % 2 == 0) return val;
+	return val * a % m;
 }
 
-int main(void) {
-	iostream::sync_with_stdio(0);
+int main() {
+	ios::sync_with_stdio(0);
 	cin.tie(0);
-
-	cin >> a >> b >> c;
-	cout << func(a, b, c);
+	cin >> A >> B >> C;
+	cout << func(A, B, C);
 }
