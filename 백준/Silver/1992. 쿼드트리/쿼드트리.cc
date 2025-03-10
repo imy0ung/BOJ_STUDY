06e8ac504f831e20e6b86c04a7d6ce5ad4fd5e00
ditto <105477780+imy0ung@users.noreply.github.com>
2025-03-10 18:28:26 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,47 +1,43 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#pragma warning (disable:4996)
 using namespace std;
-using ll = long long;
-// getline(cin,string s) => 공백을 포함한 문자 받기
-// list- insert는 앞부분, iterator 햇갈리지말자,,
-// deque vs vector, 메모리 연속성 차이, deque이 상위호환느낌
 
-string board[200];
-int n;
+// k step 때, 각 사분면의 색종이 개수를 알면 k+1일 때도 알 수 있다.
+// k 일때 4분면의 출력값을 모아보면, k+1개의 출력값이 될 것
 
-bool check(int n, int r, int c) {
-    for (int i = r; i < r + n; i++) {
-        for (int j = c; j < c + n; j++) {
-            if (board[r][c] != board[i][j])
-                return false;
-        }
+int N;
+string board[70];
+
+int check(int N, int r , int c) {
+    for (int i = r; i < r + N; i++) {
+        for (int j = c; j < c + N; j++)
+            if (board[r][c] != board[i][j]) return 0;
     }
-    return true;
+
+    return 1;
 }
 
-void func(int n, int r, int c) {
-    // base-condition : 같은 종류일 때 종료
-    if (check(n, r, c)) {
+void func(int N, int r, int c) {
+    if (check(N, r, c)) {
         cout << board[r][c];
-        int half = 1 << (n - 1);
         return;
     }
+    int point = N / 2;
     cout << '(';
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
-            func(n / 2, r + i * n / 2, c + j * n / 2);
+            func(point, r + point * i, c + point * j);
         }
     }
     cout << ')';
-    // 괄호 자체가 압축을 의미하는 건데 왜이리 돌아갔나..ㅜ
 }
 
 int main(void) {
-    iostream::sync_with_stdio(0);
+    ios::sync_with_stdio(0);
     cin.tie(0);
-    cin >> n;
-    for (int i = 0; i < n; i++)
-        cin >> board[i];
-    func(n, 0, 0);
+    
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> board[i];
+    
+    func(N, 0, 0);
 }
