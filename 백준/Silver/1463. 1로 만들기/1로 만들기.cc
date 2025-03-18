d73b0394fb0f3fe1313f4200b2a13f2f23b9e523
ditto <105477780+imy0ung@users.noreply.github.com>
2025-03-18 09:15:25 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,30 +1,25 @@
 #include <iostream>
-#include <cstdio>
 #include <algorithm>
-#include <vector>
 #include <stack>
 #include <queue>
-#include <unordered_set>
-#include <unordered_map>
-#pragma warning (disable : 4996)
 
 using namespace std;
-
-int d[1000001];
-
-int func(int n) {
-	d[1] = 0;
-	for (int i = 2; i <= n; i++) {
-		d[i] = d[i - 1] + 1;
-		if (i % 2 == 0) d[i] = min(d[i], d[i / 2] + 1);
-		if (i % 3 == 0) d[i] = min(d[i], d[i / 3] + 1);
-	}
-	return d[n];
-}
+int d[1000005];
+int n;
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int n; cin >> n;
-	cout << func(n);
+	
+	cin >> n;
+
+	d[1] = 0;
+	
+	for (int i = 2; i <= n; i++) {
+		d[i] = d[i - 1] + 1;
+		if (i % 3 == 0) d[i] = min(d[i], d[i / 3] + 1);
+		if (i % 2 == 0) d[i] = min(d[i], d[i / 2] + 1);
+	}
+
+	cout << d[n];
 }
