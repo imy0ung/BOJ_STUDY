8aa436745d05d3062c5f2537c3a04b1fc678b9e4
ditto <105477780+imy0ung@users.noreply.github.com>
2025-03-24 20:18:04 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 4012 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,31 +1,32 @@
-#include <iostream>
-#include <bits/stdc++.h>
-#pragma warning (disable:4996)
-using namespace std;
-using ll = long long;
-// getline(cin,string s) => 공백을 포함한 문자 받기
-// list- insert는 앞부분, iterator 햇갈리지말자,,
-// deque vs vector, 메모리 연속성 차이, deque이 상위호환느낌
+#include <iostream>	
+#include <algorithm>
+#include <list>
+#include <stack>
+#include <queue>
+#include <tuple>
+#define ll long long
 
-int n;
-int height = 1;
-int t[505][505];
+using namespace std;
+
 int d[505][505];
+int arr[505][505];
 
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i; j++) {
-            cin >> t[i][j];
-        }
-    }
-    d[1][1] = t[1][1];
-    for (int i = 2; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            d[i][j] = max(d[i - 1][j - 1], d[i - 1][j]) + t[i][j];
-        }
-    }
-    cout << *max_element(d[n] + 1, d[n] + n + 1);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < i; j++)
+			cin >> arr[i][j];
+	}
+	d[1][0] = arr[1][0];
+	for (int k = 1; k <= n; k++) {
+		for (int j = 0; j < k; j++) {
+			if (j == 0) d[k][j] = d[k - 1][0] + arr[k][j];
+			else if (j == k) d[k][j] = d[k - 1][k - 1] + arr[k][j];
+			else d[k][j] = max(d[k - 1][j - 1], d[k - 1][j]) + arr[k][j];
+		}
+	}
+	cout << *max_element(d[n], d[n] + n);
 }
